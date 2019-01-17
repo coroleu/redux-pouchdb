d35df2cfca39eeb8c10ec6803b525b8f9d896c57
Albert <acoroleu@gmail.com>
2019-01-17 17:50:56 +0100

    Removing from store when _deleted change arrives

--- src/persistentArrayReducer.js
+++ src/persistentArrayReducer.js
@@ -1,9 +1,10 @@
-import saveArray, { isUpToDate } from './utils/saveArray'
-import { equalsOmittingDocProps } from './utils/ramdaUtils'
+import saveArray, {isUpToDate} from './utils/saveArray'
+import {equalsOmittingDocProps} from './utils/ramdaUtils'
 import waitAvailability from './utils/waitAvailability'
 import log from './utils/log'
 
-export const UPDATE_ARRAY_REDUCER = '@@redux-pouchdb/UPDATE_ARRAY_REDUCER'
+export const UPDATE_ARRAY_REDUCER = '@@redux-pouchdb/UPDATE_ARRAY_REDUCER';
+export const REMOVE_ARRAY_REDUCER = '@@redux-pouchdb/REMOVE_ARRAY_REDUCER';
 
 let initialized = {}
 let running = {}
@@ -37,17 +38,25 @@ const updateArrayReducer = (store, doc, reducerName) => {
   })
 }
 
-const initializePersistentArrayReducer = async (
-  storeGetter,
-  db,
-  reducerName,
-  saveArrayReducer
-) => {
+const removeArrayReducer = (store, doc, reducerName) => {
+  // log('store.dispatch update array', JSON.stringify(doc, null, 2))
+  return store.dispatch({
+    type: REMOVE_ARRAY_REDUCER,
+    reducerName,
+    doc
+  })
+}
+
+
+const initializePersistentArrayReducer = async (storeGetter,
+                                                db,
+                                                reducerName,
+                                                saveArrayReducer) => {
   try {
     const store = await waitAvailability(storeGetter)
 
     // dispatch updates
-    const res = await db.allDocs({ include_docs: true })
+    const res = await db.allDocs({include_docs: true})
     await Promise.all(
       res.rows.map(row => updateArrayReducer(store, row.doc, reducerName))
     )
@@ -60,7 +69,7 @@ const initializePersistentArrayReducer = async (
       // log('change', change)
       if (change.doc) {
         // log('updateArrayReducer', change.doc)
-        updateArrayReducer(store, change.doc, reducerName)
+        change.doc._deleted ? removeArrayReducer(store, change.doc, reducerName) : updateArrayReducer(store, change.doc, reducerName)
       } else {
         // log('saveArrayReducer', store.getState())
         saveArrayReducer(store.getState())
@@ -112,6 +121,14 @@ const persistentArrayReducer = (storeGetter, db, reducerName) => reducer => {
       }
 
       return reducer(lastState, action)
+    }
+    else if (action.type === REMOVE_ARRAY_REDUCER &&
+      action.reducerName === reducerName &&
+      action.doc) {
+      lastState = state.filter(item => {
+        return !equalsOmittingDocProps(item, action.doc) && item._id !== action.doc._id
+      });
+      return reducer(lastState, action);
     }
 
     const reducedState = reducer(state, action)
